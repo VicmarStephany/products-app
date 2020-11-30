a4a92e5ee7e8ff311b01b94ebd5fb9f0e37dbce9
vicmarstephany <vicmarstephany@gmail.com>
2020-11-30 00:29:24 -0400

    update

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/prueba'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/prueba/index.html'));});
+app.use(express.static('/dist/prueba'));
+/*
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/prueba/index.html'));
+});
+*/
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', function (req, res) {
+    res.sendFile('index.html', { root: 'dist/<prueba/' }
+    );
+});
